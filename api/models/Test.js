7453f27427a012f56c123cd2bd93cbe558a2cd10
tuannvm <tuanpro.itman@gmail.com>
2015-06-22 00:05:26 +0700

    aa

--- api/models/Test.js
+++ api/models/Test.js
@@ -1,3 +1,4 @@
+var bcrypt = require('bcrypt');
 /**
 * Test.js
 *
@@ -5,10 +6,41 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
-module.exports = {
-
+var Test = {
+  adapter: 'disk',
   attributes: {
+    name: {type: 'string'},
+    encryptName: {type: 'string'},
+    date: {type: 'string'},
+    creator: {type: 'string'},
+    content: {type: 'string'}
+  },
 
+  index: function (inputs,cb) {
+    Test.create({
+      name: inputs.name,
+      date: inputs.date,
+      creator: inputs.creator,
+      content: inputs.content
+    })
+      .exec(cb);
+  },
+  show: function (cb) {
+    Test.find().exec(cb);
+  },
+  // Lifecycle Callbacks
+  beforeCreate: function (values, cb) {
+
+    // Encrypt password
+    bcrypt.hash(values.name, 10, function(err, hash) {
+      if(err) return cb(err);
+      values.name = hash;
+      console.log(hash);
+      //calling cb() with an argument returns an error. Useful for canceling the entire operation if some criteria fails.
+      cb();
+    });
   }
 };
 
+
+module.exports = Test;
